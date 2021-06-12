113d174874fd22e4b8286a923d3ef12918ab30dc
toshi <toshi@toshi.click>
2021-06-13 02:48:23 +0900

    xls読み込み追加

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import pandas_datareader.data as web
+import time
+import sys
+
+import urllib2
 
 import requests
 import json
@@ -9,12 +13,15 @@ response = requests.get(endPoint + path)
 print('現在のビットコインの価格は', response.json()['data'][0]['last'],'円です')
 
 # JPXの東証上場一覧のページへのアクセス
-# !wget 'https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls' -O data_j.xls -a wget-log
+
+data = urllib2.urlopen("https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls")
+with open("data_j.xls", "wb") as f:
+    f.write(data.read())
 
 ## pandasでexcelファイルの読み込みとdf整形など
-# company_df = pd.read_excel("data_j.xls")
-# company_df.columns = ['date', 'code', 'name', 'lst', 'sectorCode', 'sectorName', 'flr1', 'flr2', 'flr3', 'flr4']
-# drop_col = ['flr1', 'flr2', 'flr3', 'flr4']
-# company_df = company_df.drop(drop_col, axis=1)  # 不要な列の削除
-#
+company_df = pd.read_excel("data_j.xls")
+company_df.columns = ['date', 'code', 'name', 'lst', 'sectorCode', 'sectorName', 'flr1', 'flr2', 'flr3', 'flr4']
+drop_col = ['flr1', 'flr2', 'flr3', 'flr4']
+company_df = company_df.drop(drop_col, axis=1)  # 不要な列の削除
+
 # j = 0
