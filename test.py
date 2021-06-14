736a8fe67a7e877ca787561c751057edfbc360ea
toshi <toshi@toshi.click>
2021-06-14 19:23:25 +0900

    xls読み込み修正

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import pandas_datareader.data as web
 import time
+import os
 import sys
-
-import urllib2
+import datetime as dt
 
 import requests
 import json
@@ -14,9 +14,14 @@ print('現在のビットコインの価格は', response.json()['data'][0]['las
 
 # JPXの東証上場一覧のページへのアクセス
 
-data = urllib2.urlopen("https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls")
-with open("data_j.xls", "wb") as f:
-    f.write(data.read())
+data = requests.get("https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls")
+contentType = response.headers['Content-Type']
+contentDisposition = response.headers['Content-Disposition']
+ATTRIBUTE = 'filename='
+fileName = 'data_j.xls'
+saveFilePath = os.path.join('.', fileName)
+with open(saveFilePath, 'wb') as saveFile:
+    saveFile.write(response.content)
 
 ## pandasでexcelファイルの読み込みとdf整形など
 company_df = pd.read_excel("data_j.xls")
@@ -25,3 +30,33 @@ drop_col = ['flr1', 'flr2', 'flr3', 'flr4']
 company_df = company_df.drop(drop_col, axis=1)  # 不要な列の削除
 
 # j = 0
+count = 0
+tmp_code = 0
+for index, item in company_df.iterrows():
+    if j <= index / (len(company_df) / 20) < j + 1:
+        if count == 0:
+            print('from:'+str(item['code'])+' start!')
+            count = 1
+        ### 中身を文字型に変換して'.JP'を付与
+        code = str(item['code']) + '.JP'
+        stock_df = web.DataReader(code, "stooq")
+        # 早すぎると規制されるっぽいのでsleep
+        time.sleep(1)
+
+        # 1日の制限超えた応答きたらエラー
+        if stock_df.empty:
+            print(stock_df)
+            print('error:' + str(item['code']) + ' daily over!?')
+        else:
+            stock_df['Code'] = item['code']
+
+            # 後処理のために明示的にカラムを並び替える
+            stock_df.reindex(columns=['Code', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+
+            # csv保存：[stock_code].csv
+            filename = "/content/drive/My Drive/stock/" + str(item['code']) + ".csv"
+            stock_df.to_csv(filename, encoding="utf-8")
+            tmp_code = item['code']
+
+print('to:'+str(tmp_code)+' end!')
+print('All_finish!')
